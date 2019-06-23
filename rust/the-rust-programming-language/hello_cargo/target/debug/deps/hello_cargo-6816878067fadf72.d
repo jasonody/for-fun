ff6c29ef266855d0d0edc862d77c6bb89e746c3f
Jason Ody <jasonody@users.noreply.github.com>
2019-06-23 14:20:38 -0700

    introduction to using cargo

---
+++ rust/the-rust-programming-language/hello_cargo/target/debug/deps/hello_cargo-6816878067fadf72.d
@@ -0,0 +1,5 @@
+/Users/jasonody/Repos/for-fun/rust/the-rust-programming-language/hello_cargo/target/debug/deps/hello_cargo-6816878067fadf72.rmeta: src/main.rs
+
+/Users/jasonody/Repos/for-fun/rust/the-rust-programming-language/hello_cargo/target/debug/deps/hello_cargo-6816878067fadf72.d: src/main.rs
+
+src/main.rs:
