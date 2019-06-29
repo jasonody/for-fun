0d64c2d25c4695f0f6117517ad5179bd2dcc486b
Jason Ody <jasonody@users.noreply.github.com>
2019-06-29 11:22:43 -0700

    string serialization using a delineator

--- other/string-serialization.js
+++ other/string-serialization.js
@@ -11,7 +11,7 @@ function serialize(strings) {
 }
 
 //Write a function that takes a the output from above and returns the original collection of strings
-//9:the first10:the second14:the long third
+///9:the fi,st11:2the sec$,d17:the long th,,d33,
 function deserialize(input) {
   const MODES = {
     READ_STRING: 'READ',
@@ -53,7 +53,7 @@ function deserialize(input) {
 }
 
 //Write a function that takes a the output from above and returns the original collection of strings
-//9:the first11:2the second17:the long third333
+//9:the fi,st11:2the sec$,d17:the long th,,d33,
 //          11111111112222222222333333333344444 
 //012345678901234567890123456789012345678901234
 function deserialize1(input) {
@@ -71,11 +71,65 @@ function deserialize1(input) {
   return strings;
 }
 
-const strings = ['the first', '2the second', 'the long third333'];
+//Using a delineator
+const DELINEATOR = ',';
+const ESCAPE_CHAR = '$';
+
+//Write a function that takes a collection of strings and returns a single string
+function serialize_with_delineator(strings) {
+  let output = '';
+
+  for (const string of strings) {
+    const escaped = string.replace(new RegExp(DELINEATOR, 'g'), `${ESCAPE_CHAR}${DELINEATOR}`);
+    output = `${output}${escaped}${DELINEATOR}`;
+  }
+
+  return output.slice(0, -1); //remove the trailing delineator
+}
+
+//Write a function that takes a the output from above and returns the original collection of strings
+//the fi$,st,2the sec$$,d,the long th$,$,d33,,
+function deserialize_with_delineator(input) {
+  const strings = [];
+  let string = '';
+  let isEscaped = false;
+
+  for (const char of input) {
+    if (char === ESCAPE_CHAR) {
+      if (isEscaped) {
+        string = `${string}${char}`;
+      } else {
+        isEscaped = true;
+      }
+    } else if (char === DELINEATOR) {
+      if (isEscaped) {
+        string = `${string}${char}`;
+        isEscaped = false;
+      } else {
+        strings.push(string);
+        string = '';
+      }
+    } else {
+      if (isEscaped) { //current char is not delineator so write escape char before current char
+        string = `${string}${ESCAPE_CHAR}`;
+        isEscaped = false;
+      }
+      string = `${string}${char}`;
+    }
+  }
+
+  strings.push(string);
+
+  return strings;
+}
+
+const strings = ['the fi,st', '2the sec$,d', 'the long th,,d33,'];
 
 const serialized = serialize(strings);
 console.log(serialized);
-
-const deserialized = deserialize(serialized);
 console.log(deserialize(serialized));
 console.log(deserialize1(serialized));
+
+const serialized_delineator = serialize_with_delineator(strings);
+console.log(serialized_delineator);
+console.log(deserialize_with_delineator(serialized_delineator));
