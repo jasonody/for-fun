c8a325c0161997070dda4a4c5059bba387229d94
Jason Ody <jasonody@users.noreply.github.com>
2019-08-26 18:48:09 -0700

    tidy up

--- technique/currying.js
+++ technique/currying.js
@@ -1,14 +1,14 @@
 const readProp = prop => obj => obj[prop]
-const aReader = readProp('a')
-const bReader = readProp('b')
-
 const map = fn => xs => xs.map(fn)
 
 const obj1 = { a: 'obj1 a', b: 'obj1 b'}
 const obj2 = { a: 'obj2 a', b: 'obj2 b'}
-
 const objs = [obj1, obj2]
 
+const aReader = readProp('a')
+const bReader = readProp('b')
+
+const a = aReader(obj1) // 'obj1 a'
 const aMap = map(aReader)
-const onlyA = aMap(objs)
-const onlyB = map(bReader)(objs)
+const onlyA = aMap(objs) // [ 'obj1 a', 'obj2 a' ]
+const onlyB = map(bReader)(objs) // [ 'obj1 b', 'obj2 b' ]
